ea9fff1c95a96c6b0963082374d0b7345bb33ca7
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-02-15 01:08:51 -0300

    feat: integrate modules and add word selection menu
    - Add mp.input.select for word selection menu
    - Connect dictionary module for fetching definitions
    - Connect html module for generating output

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 local utils = require 'mp.utils'
+local input = require 'mp.input'
+local html = require 'html'
+local dictionary = require 'dictionary'
 
 function main()
   local sub_text = mp.get_property("sub-text")
@@ -12,6 +15,19 @@ function main()
   for word in string.gmatch(result.stdout, "[^%s]+") do
     table.insert(words, word)
   end
+
+  input.select({
+    items = words,
+    submit = function(index)
+      local def = dictionary.fetch_definition(words[index])
+      if not def then
+        print("No definition found")
+        return
+      end
+      html.generate_html(def)
+    end
+  })
+
 end
 
 mp.add_key_binding("a", "print-sub", main)
