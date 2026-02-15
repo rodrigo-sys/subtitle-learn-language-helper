0235c7f15a64cecf2879938cb4c6f506c9841b3b
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-02-15 01:07:26 -0300

    feat: add subtitle retrieval and tokenization
    - Get current subtitle via mp.get_property
    - Tokenize using sudachi command
    - Split output into words array

--- main.lua
+++ main.lua
@@ -1,4 +1,17 @@
+local utils = require 'mp.utils'
+
 function main()
+  local sub_text = mp.get_property("sub-text")
+
+  local result = utils.subprocess({
+    args = {"sh", "-c", "echo '" .. sub_text .. "' | sudachi -w -m c"},
+    capture_stdout = true
+  })
+
+  local words = {}
+  for word in string.gmatch(result.stdout, "[^%s]+") do
+    table.insert(words, word)
+  end
 end
 
 mp.add_key_binding("a", "print-sub", main)
